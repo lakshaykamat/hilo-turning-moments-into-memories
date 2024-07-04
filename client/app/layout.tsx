8872f5859dbe8d8e3d5cad601ed14a94f82d0b8d
Lakshay Kamat <lakshaykamat2048@gmail.com>
2024-07-04 19:17:05 +0530

    Meta tags added

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -24,6 +24,40 @@ function RootLayout({
     <html lang="en">
       <head>
         <title>SyncTalk</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="SyncTalk - A chat and post-sharing platform where users can share posts and engage through likes, comments, and shares."
+        />
+        <meta
+          name="keywords"
+          content="SyncTalk, chat application, post sharing, social media, messaging, like, comment, share"
+        />
+        <meta name="author" content="Lakshay Kamat" />
+        <meta name="robots" content="index, follow" />
+        <meta
+          property="og:title"
+          content="SyncTalk - Chat and Post Sharing Platform"
+        />
+        <meta
+          property="og:description"
+          content="Join SyncTalk to share posts, chat with friends, and engage with the community through likes, comments, and shares."
+        />
+        <meta property="og:image" content="URL_to_your_image" />
+        <meta property="og:url" content="https://synctalk.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="SyncTalk - Chat and Post Sharing Platform"
+        />
+        <meta
+          name="twitter:description"
+          content="Join SyncTalk to share posts, chat with friends, and engage with the community through likes, comments, and shares."
+        />
+        <meta name="twitter:image" content="URL_to_your_image" />
+        <meta name="twitter:site" content="@lakshaykamat" />
       </head>
       <body>
         <AuthProvider>
